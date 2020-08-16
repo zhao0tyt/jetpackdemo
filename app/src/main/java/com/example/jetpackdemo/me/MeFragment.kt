03d6c90c27a8bf51b927b187b92b9a9d1196eaaf
zhaozhiqiang <517484480@qq.com>
2020-08-16 22:21:16 +0800

    me fragment layout

--- app/src/main/java/com/example/jetpackdemo/me/MeFragment.kt
+++ app/src/main/java/com/example/jetpackdemo/me/MeFragment.kt
@@ -7,16 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.jetpackdemo.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
